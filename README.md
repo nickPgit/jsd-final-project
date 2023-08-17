71b1553af0683de892f0dbb990d76d273d872660
Nick Patane <nickpatane@icloud.com>
2023-08-17 14:12:49 +1000

    updated styling and functionality

--- README.md
+++ README.md
@@ -1,14 +1,54 @@
-# final project 
+# H1 final project - learning to read
+# H2 JSD final project
 
-this app is designed to assist parents to teach pre-kinder children to read. 
+**Try it out here:** https://nickpgit.github.io/jsd-final-project/
 
-View my live project here: https://nickpgit.github.io/jsd-final-project/
+**overview**
+This app is designed to assist parents to teach pre-kinder children to read (and particular to develop 'emergent reading', the first stages of reading development). It aims to do this through: 
 
-the app aims to develop 'emergent reading'; the first stages of reading development, consisting of: 
-1. identifying words that rhyme
-2. counting the number of syllables in a word 
-3. recognizing words with the same beginning sound
-4. break a sentence into words
-5. recognise and 'play with' the individual sounds in spoken words (phonemic awareness) 
+1. linking the sound of a letter with the letter itself
+2. identifying and saying words that start with that letter, emphasizing the sound of the first letter
+3. identifying words that rhyme with a particular word
+4. identifying an animal whose name starts with the chosen letter
+5. recognise and 'play with' the individual sounds in spoken words (phonemic awareness) . 
 
-  
+**How it works:** 
+The app uses AJAX requests to obtain information about a clicked letter, including words that start with that letter and rhyming words. and populate the UI accordingly. The app uses the Web Speech API to say any letter or word that is clicked, for an interactive learning experience. 
+
+In practice: 
+1. The user (the child's parent) selects a letter
+2. Parent to say the sound of the letter aloud and child to repeat. Recite the main word shown on the screen for that letter. Child can click on any and the program will say the word. Encourage child to repeat words and sounds
+3. Parent: "what are some words that start with (letter sound)? 
+4. Click 'words starting with' button to move to next page; 
+5. Parent and child say those words aloud, putting emphasis on the sound of the first letter
+6. Parent: "what are some words that rhyme with (the main word)? Click 'words rhyming with' button to move to next page; 
+7. Parent and child say those words aloud, putting emphasis on the sound of the words and how they rhyme. Parent navigates to animal page. 
+8. Parent: this animals name starts with (letter sound), which animal is it? Parent and child say name of animal, putting emphasis on the sound of the first letter
+9. revisit any sections that the child did not understand or had trouble saying
+10. repeat above steps for another letter (you can see which letter have already been viewed)
+
+**screenshots:** 
+![main page](images/main.png)
+![letter and sound](images/letter%20and%20sound.png)
+
+**Technical hurdles:** 
+- I originally used the flickr API to return images of each word when clicked, however the results were inconsistent and sometimes not appropriate, so I chose to remove this feature.
+- the animal image obtained through the flickr API is sometimes not correct. I had trouble trying to find an appropriate API to get consistent images from, and also wasn't able to find one for drawings/cartoon pictures
+- when deployed as a live site, the 'voice' of the speech API changes depending on the device. I tried to set the voice to 'UK female' to get an Australian-sounding accent. I removed setting the voice and instead let it default to 'US male' to ensure it says words correctly
+- I struggled with overthinking the possibilities of what the app could do
+- I was unable to find a way to make the app say the sounds of letters
+- I was unable to work out how to clean up the code for the 
+
+**What I learned**
+- I needed to have more definite plans for the app before creating it. This will keep me on track and help me not get bogged down in the various directions I could take the app in 
+- API results can be very inconsistent and sometimes the results data needs to be filtered. Some of the images and words were inappropriate
+
+**Technology that we haven't covered in class:** 
+The Web Speech API - I used this inbuilt browser API to 'say' the words or letters that were clicked. I was limited by the voices changing across devices, which meant that I had to use the default american voice rather than a more relevant voice for Australian users. 
+
+**future features** 
+- find a way for the app to say the sounds of the letters, not just the name of the letter
+- use speech recognition to check whether the child says the sound of the letter or says the word correctly 
+- make this into an actual game, to make it more engaging, with cartoon characters, letters that move around the screen, allowing the child to interactively experiment with the sounds of letter, perhaps allowing them to join letters or sounds together to form new sounds and words. 
+- research the reading teaching methods further, to be more clear about exactly how the app should be used by parents to teach children. 
+- 
--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // the alphabet as an array
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
-
+// array of animals for each letter
 const animals = ['alligator','bilby','cheetah','dingo','echidna','flying fox','goose','hamster','iguana','jellyfish','koala','lion','magpie','narwhal','octopus','platypus','quoll','rosella','skink','tiger','ulysses butterfly','vervet monkey','wombat','x-ray tetra','yabby','zebra'];
 
-// speechSynthesis const to access text to speech
+// access text to speech through the Web Speech API
 const synth = window.speechSynthesis; 
 
 // speak the text that is passed in to this function using the web speech API
 const speakText = function(text) {
-    const speech = new SpeechSynthesisUtterance(text); // create a new 'utterance'
+    const speech = new SpeechSynthesisUtterance(text); // create new 'utterance'
     speech.rate = 0.8; // alter rate of speech
-    speech.voice = voices[5]; // select voice
-    synth.speak(speech); // speak the utterance
+    speech.voice = voices[5]; // set voice to 'Google UK English Female'
+    synth.speak(speech); // speak the 'utterance'
 };
 
-// get the available voices after 50 ms (allow them to load)
-let voices; 
+// get the available voices after 50 ms (allow them to load first)
+let voices;
 setTimeout(() => {
     voices = synth.getVoices()
 }, 50);
@@ -26,17 +26,20 @@ const wordsStartingWithButton = document.querySelector("#wordsStartingWith");
 const wordsRhymingWithButton = document.querySelector("#wordsRhymingWith");
 const animalsButton = document.querySelector("#animals");
 // declare variables for containers
-const letterContainer = document.querySelector(".letterContainer"); // for the alphabet
+const letterContainer = document.querySelector(".letterContainer"); 
 const mainPageContainer = document.querySelector(".mainPageContainer"); 
 const wordsStartingWithContainer = document.querySelector(".wordsStartingWith");
 const wordsRhymingWithContainer = document.querySelector(".wordsRhymingWith");
 const animalsContainer = document.querySelector(".animals");
+const instructionsContainer = document.querySelector(".instructions");
+const cat = document.querySelector("#cat");
 
 // create variables to keep track of and alter the following
 let selectedLetter = '';
 let mainWord = '';
 let matchingWords = []; 
 let rhymingWords = []; 
+let letters = []; 
 
 // create a visible letter for each letter in the alphabet at the top of screen
 alphabet.forEach( letter => {
@@ -46,22 +49,33 @@ alphabet.forEach( letter => {
     letterContainer.appendChild(letterDiv)
     })
 
-// get all of the letters (divs)
+// get all of the letters (divs) after they have been loaded
 const letterDivs = document.querySelectorAll(".letter") 
-// keep track of all the words 
-const wordDivs = []; 
-const viewedLetters = []
+setTimeout(() => {
+    letterDivs.forEach(letter => { 
+        letter.style.animation = "word 5s";
+    })
+}, 2000);
 
 // left and right divs of the main display section
-const leftLetterDiv = document.querySelector("#left") // holds upper and lowercase 
+const leftLetterDiv = document.querySelector("#left") // holds upper and lowercase letter
 const rightLetterDiv = document.querySelector("#right") // holds word example
+
+// function to update the display with the words generated from the AJAX request
+const updateWords = function(word, container) { 
+    const wordDiv = document.createElement('div'); 
+    wordDiv.className= "word"; 
+    wordDiv.id = word.word; 
+    wordDiv.innerHTML += `${word.word}`;
+    container.appendChild(wordDiv) 
+}
 
 //populate all display pages when a letter is clicked
 letterDivs.forEach((letter, index) => {
     letter.addEventListener('click', function(ev) {
         selectedLetter = alphabet[index]
+        
         ev.target.className = 'letter selected'
-        viewedLetters.push(selectedLetter)
         speakText(selectedLetter); 
         leftLetterDiv.innerHTML = ''
         rightLetterDiv.innerHTML = ''
@@ -74,6 +88,14 @@ letterDivs.forEach((letter, index) => {
         const bigLetterDiv = document.createElement('div'); 
         bigLetterDiv.innerHTML = selectedLetter.toUpperCase() + " " + selectedLetter
         leftLetterDiv.appendChild(bigLetterDiv)
+        
+        const letterArray = Array.from(letterDivs)
+        let check = letterArray.every((letter) => letter.className === "letter selected")
+        if (check) { 
+            instructionsContainer.innerHTML += `<br><br>WELL DONE! You have completed every letter!`
+            cat.style.display = "block";
+            speakText('WELL DONE! You have completed every letter!')
+        }
 
         // get one 3-letter word starting with the clicked letter, sorted by 'popularity'
         axios.get(`https://api.datamuse.com/words?sp=${selectedLetter}??`) 
@@ -84,20 +106,14 @@ letterDivs.forEach((letter, index) => {
                 wordsRhymingWithButton.innerHTML = `words rhyming with <strong>${mainWord}</strong>`
                 wordDiv.innerHTML += mainWord;
                 rightLetterDiv.appendChild(wordDiv) 
-                wordDivs.push(wordDiv);
 
-                // get words rhyming with the main word, and put them in the appropriate page
+                // get words rhyming with the 'main word' example, and put them in the appropriate page
                 axios.get(`https://api.datamuse.com/words?rel_rhy=${mainWord}`) 
                 .then( res => { 
-                    rhymingWords = res.data.slice(0,3)
-
+                    // get first 3 words from results
+                    rhymingWords = res.data.slice(0,3) 
                     rhymingWords.forEach(word => { 
-                        const wordDiv = document.createElement('div'); 
-                        wordDiv.className= "word"; 
-                        wordDiv.id = word.word; 
-                        wordDiv.innerHTML += `${word.word}`;
-                        wordsRhymingWithContainer.appendChild(wordDiv) 
-                        wordDivs.push(wordDiv);
+                        updateWords(word, wordsRhymingWithContainer); 
                     })                
                 })
                 .catch(err => { 
@@ -113,12 +129,7 @@ letterDivs.forEach((letter, index) => {
         .then( res => { 
             matchingWords = res.data.slice(0,3)
             matchingWords.forEach(word => { 
-                const wordDiv = document.createElement('div'); 
-                wordDiv.className= "word"; 
-                wordDiv.id = word.word; 
-                wordDiv.innerHTML += `${word.word[0]}${word.word[1]}${word.word[2]}`;
-                wordsStartingWithContainer.appendChild(wordDiv) 
-                wordDivs.push(wordDiv);
+                updateWords(word, wordsStartingWithContainer); 
             })
         })
         .catch(err => { 
@@ -146,40 +157,23 @@ letterDivs.forEach((letter, index) => {
             animalsContainer.appendChild(img)
         })
         .catch(err => { 
-            console.log('error: ', err)
+            console.log('flickr request error: ', err)
         })
     })
 });
 
-
-mainPageContainer.addEventListener('click', function(ev) { 
-    speakText(ev.target.innerHTML); 
-    
-    // axios.get('https://www.flickr.com/services/rest/', {
-    //     params: { 
-    //         method: 'flickr.photos.search',
-    //         text: `${ev.target.innerHTML}`,
-    //         format: 'json',
-    //         nojsoncallback: 1, 
-    //         api_key: '2f5ac274ecfac5a455f38745704ad084',
-    //         safe_search: 1,
-    //         privacy_filter: 1,
-    //         sort: "interestingness-desc"
-    //     }
-    // })
-    //     .then(res => { 
-    //         console.log(res.data)
-    //         const picture = ev.target
-    //         const img = document.createElement('img')
-    //         img.src = generateImageUrl(res.data.photos.photo[0])
-    //         picture.appendChild(img)
-    //     })
-        // .catch(err => { 
-        //     console.log('error: ', err)
-        // })
-})
+// generate an image from the provided photo details 
+const generateImageUrl = (photo, size='q') => { 
+    return `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
+}
 
 // speak the text of each of the containers ('pages')
+leftLetterDiv.addEventListener('click', function(ev) { 
+    speakText(ev.target.innerHTML[0]); 
+})
+rightLetterDiv.addEventListener('click', function(ev) { 
+    speakText(ev.target.innerHTML); 
+})
 wordsStartingWithContainer.addEventListener('click', function(ev) { 
     speakText(ev.target.innerHTML); 
 })
@@ -190,24 +184,27 @@ animalsContainer.addEventListener('click', function(ev) {
     speakText(ev.target.id); 
 })
 
-// generate an image from the provided photo details 
-const generateImageUrl = (photo, size='q') => { 
-    return `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${size}.jpg`;
-}
-
-// 'change pages' of the UI display when each button is clicked
-wordsStartingWithButton.addEventListener('click', function(ev){ 
-    mainPageContainer.style.display = "none"
-    wordsStartingWithContainer.style.display = "block"
-    wordsRhymingWithContainer.style.display = "none"
-    animalsContainer.style.display = "none"
-})
-
+// 'change pages' and instructions in the UI display when each button is clicked
 letterAndSoundButton.addEventListener('click', function(ev){ 
     mainPageContainer.style.display = "flex"
     wordsStartingWithContainer.style.display = "none"
     wordsRhymingWithContainer.style.display = "none"
     animalsContainer.style.display = "none"
+
+    instructionsContainer.innerHTML = `1. Parent selects a letter <br>
+    2. Parent to say the sound of the letter aloud and child to repeat. Recite the main word shown on the screen for that letter. Child can click on any word and the program will say the word. Encourage child to repeat words and sounds. Use the word in a simple sentence with the child
+    <br>3. Parent: "what are some words that start with (letter sound)? 
+    <br>4. Click 'words starting with' button to move to next page`
+})
+
+wordsStartingWithButton.addEventListener('click', function(ev){ 
+    mainPageContainer.style.display = "none"
+    wordsStartingWithContainer.style.display = "block"
+    wordsRhymingWithContainer.style.display = "none"
+    animalsContainer.style.display = "none"
+
+    instructionsContainer.innerHTML = `5. Parent and child say those words aloud, putting emphasis on the sound of the first letter
+    <br>6. Parent: "what are some words that rhyme with (the main word)? Click 'words rhyming with' button to move to next page`
 })
 
 wordsRhymingWithButton.addEventListener('click', function(ev){ 
@@ -215,6 +212,8 @@ wordsRhymingWithButton.addEventListener('click', function(ev){
     wordsStartingWithContainer.style.display = "none"
     wordsRhymingWithContainer.style.display = "block"
     animalsContainer.style.display = "none"
+
+    instructionsContainer.innerHTML = `7. Parent and child say those words aloud, putting emphasis on the sound of the words and how they rhyme. <br>Parent navigates to animal page. `
 })
 
 animalsButton.addEventListener('click', function(ev){ 
@@ -222,4 +221,8 @@ animalsButton.addEventListener('click', function(ev){
     wordsStartingWithContainer.style.display = "none"
     wordsRhymingWithContainer.style.display = "none"
     animalsContainer.style.display = "block"
+
+    instructionsContainer.innerHTML = `8. Parent: this animals name starts with (letter sound), which animal is it? Parent and child say name of animal, putting emphasis on the sound of the first letter
+    <br>9. revisit any sections that the child did not understand or had trouble saying
+    <br>10. repeat above steps for another letter (you can see which letters have already been viewed)`
 })
